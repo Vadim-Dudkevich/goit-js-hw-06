a8e57c804fc9d1cd826418a4ce68524db0751824
Vadim-Dudkevich <87948341+Vadim-Dudkevich@users.noreply.github.com>
2021-12-07 20:18:47 +0300

    Add files via upload

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,11 @@
-const inputRangeEl = document.getElementById('font-size-control');
-const inputTextEl = document.getElementById('text');
-
-inputRangeEl.oninput = function () {
-  inputTextEl.style.fontSize = inputRangeEl.value + 'px';
+const refs = {
+  inputRangeEl: document.querySelector('#font-size-control'),
+  inputTextEl: document.querySelector('#text'),
 };
+
+refs.inputRangeEl.addEventListener('input', onChangeFontSize);
+refs.inputTextEl.style.fontSize = `${refs.inputRangeEl.value}px`;
+
+function onChangeFontSize(event) {
+  refs.inputTextEl.style.fontSize = `${event.currentTarget.value}px`;
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,16 +1,33 @@
-const changeColor = document.querySelector('.change-color');
-const showColor = document.querySelector('.color');
-
-changeColor.addEventListener('click', event => {
-  document.body.style.backgroundColor = getRandomHexColor();
-});
-
-changeColor.addEventListener('click', event => {
-  showColor.textContent = getRandomHexColor();
-});
-
-// showColor.style.
+const changeColor = {
+  changeColorEl: document.querySelector('.change-color'),
+  selectedColorEl: document.querySelector('.color'),
+};
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function onColorChange(event) {
+  const colorHex = getRandomHexColor();
+  document.body.style.backgroundColor = colorHex;
+  changeColor.selectedColorEl.textContent = colorHex;
+}
+
+changeColor.changeColorEl.addEventListener('click', onColorChange);
+
+// !=========================
+
+// const changeColor = document.querySelector('.change-color');
+// const showColor = document.querySelector('.color');
+
+// changeColor.addEventListener('click', event => {
+//   document.body.style.backgroundColor = getRandomHexColor();
+// });
+
+// changeColor.addEventListener('click', event => {
+//   showColor.textContent = getRandomHexColor();
+// });
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
